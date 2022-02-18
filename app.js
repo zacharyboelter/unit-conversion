edfab03cf74533fc4edd0a5fbbb58b8aff70bbf2
Zachary Boelter <zboelter@gmail.com>
2022-02-18 15:20:35 -0600

    DOM / javascript done?

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 //Convert from Kilograms to Pounds 	lb=kg*2.2046
 //Convert from Feet to Meters 	m=ft/3.2808
 //Convert from Meters to Feet 	ft=m*3.2808
-
+//Convert from Gallons to Litres G=L*3.7854
+//Convert from Litres to Gallons L=G/3.7854
 
 
 let newInput = document.getElementsByClassName('inputNum')
@@ -10,8 +11,8 @@ let meter = document.getElementById('outputMetres')
 let feet = document.getElementById('outputFeet')
 let pound = document.getElementById('outputPounds')
 let kilo = document.getElementById('outputKilo')
-
-
+let gallon = document.getElementById('outputGallons')
+let litre = document.getElementById('outputLitres')
 
 function setInput(valNum) {
     for(let i = 0; i < newInput.length; i++) {
@@ -27,6 +28,14 @@ function feetConverter(valNum) {
 
 function metreConverter(valNum) {
     feet.innerHTML = valNum * 3.2808;
+}
+
+function gallonConverter(valNum) {
+    gallon.innerHTML = valNum * 3.7854;
+}
+
+function litreConverter(valNum) {
+    litre.innerHTML = valNum / 3.7854;
 }
 
 function kiloConverter(valNum) {
